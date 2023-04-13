114f735f5505b3bb84bafd338b07c434147732a1
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-04-12 22:45:50 -0600

    Limit to 8 build processes

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd Thirdparty/DBoW2
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j8
 
 cd ../../g2o
 
@@ -13,7 +13,7 @@ echo "Configuring and building Thirdparty/g2o ..."
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j
+make -j8
 
 cd ../../../
 
